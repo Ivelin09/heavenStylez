55ba3f02f0f08eeb039c851b7f6ae604657a28a1
Ivelin09 <galinaliobomirova@gmail.com>
2023-10-01 21:52:25 +0300

    init page

--- src/Router.js
+++ src/Router.js
@@ -1,20 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from './components/Main'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
